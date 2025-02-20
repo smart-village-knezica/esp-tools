4347bcf5783fdf3baf7d9ee9dbf57cd8d0e7ead0
Mladen Knezic <mladen.knezic@etf.unibl.org>
2025-02-20 19:27:49 +0100

    Fixed the incorrect script commands
    Signed-off-by: Mladen Knezic <mladen.knezic@etf.unibl.org>

--- scripts/get-tools.cmd
+++ scripts/get-tools.cmd
@@ -5,5 +5,11 @@ if defined MSYSTEM (
     goto :eof
 )
 
-call curl https://github.com/smart-village-knezica/esp-tools/releases/download/v0.1.0-rc1/windows-tools-v0.1.0.zip
-call tar windows-tools-v0.1.0.zip
+:: Download the tools from the server
+call curl -LO https://github.com/smart-village-knezica/esp-tools/releases/download/v0.1.0-rc1/windows-tools-v0.1.0.zip
+:: Extract to the current location
+call tar -xf windows-tools-v0.1.0.zip
+:: Remove the ZIP archive
+if exist windows-tools-v0.1.0.zip (
+    del windows-tools-v0.1.0.zip
+)
