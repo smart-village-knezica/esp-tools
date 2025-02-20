baf2756aa4c0362df07f288bc8404ef5a575ba5b
Mladen Knezic <mladen.knezic@etf.unibl.org>
2025-02-20 19:34:27 +0100

    Fix link name
    Signed-off-by: Mladen Knezic <mladen.knezic@etf.unibl.org>

--- scripts/get-tools.cmd
+++ scripts/get-tools.cmd
@@ -6,7 +6,7 @@ if defined MSYSTEM (
 )
 
 :: Download the tools from the server
-call curl -LO https://github.com/smart-village-knezica/esp-tools/releases/download/v0.1.0-rc1/windows-tools-v0.1.0.zip
+call curl -LO https://github.com/smart-village-knezica/esp-tools/releases/download/v0.1.0-rc2/windows-tools-v0.1.0.zip
 :: Extract to the current location
 call tar -xf windows-tools-v0.1.0.zip
 :: Remove the ZIP archive
