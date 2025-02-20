17d16498132126de7ed4192b14968db0ce88e071
Mladen Knezic <mladen.knezic@etf.unibl.org>
2025-02-20 18:15:18 +0100

    Fix incorrect download link
    Signed-off-by: Mladen Knezic <mladen.knezic@etf.unibl.org>

--- scripts/get-tools.cmd
+++ scripts/get-tools.cmd
@@ -5,5 +5,5 @@ if defined MSYSTEM (
     goto :eof
 )
 
-call curl https://github.com/smart-village-knezica/esp-tools/releases/download/windows-tools-v0.1.0.zip
+call curl https://github.com/smart-village-knezica/esp-tools/releases/download/v0.1.0-rc1/windows-tools-v0.1.0.zip
 call tar windows-tools-v0.1.0.zip
