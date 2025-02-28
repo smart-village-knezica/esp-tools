8a03e9f6308d31370a37da286b3bfa5ea30e851c
Mladen Knezic <mladen.knezic@etf.unibl.org>
2025-02-28 13:38:05 +0100

    Updated download script to match new release version
    Signed-off-by: Mladen Knezic <mladen.knezic@etf.unibl.org>

--- scripts/get-tools.sh
+++ scripts/get-tools.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Download the tools from the server
-wget https://github.com/smart-village-knezica/esp-tools/releases/download/v0.2.0/linux-tools-v0.2.0.tar.gz
+wget https://github.com/smart-village-knezica/esp-tools/releases/download/v0.2.1/linux-tools-v0.2.1.tar.gz
 # Extract to the current location
-tar -xf linux-tools-v0.2.0.tar.gz
+tar -xf linux-tools-v0.2.1.tar.gz
 # Remove the downloaded archive
-if [ -f "linux-tools-v0.2.0.tar.gz" ]; then
-    rm linux-tools-v0.2.0.tar.gz
+if [ -f "linux-tools-v0.2.1.tar.gz" ]; then
+    rm linux-tools-v0.2.1.tar.gz
 fi
 
